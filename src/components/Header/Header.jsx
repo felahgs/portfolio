f236b8ee9fcb174fde5952eeaf30495736392006
Felipe Souza <fgsouza93@hotmail.com>
2021-04-16 00:25:10 -0300

    Refactor header section

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Navbar from "../Navbar/Navbar";
+import PropTypes from "prop-types";
 import "./style.css";
 
 const socialLinks = [
@@ -19,52 +20,57 @@ const SocialLinksContainer = () =>
     </li>
   ));
 
+const ContainerContent = ({ name, prefix, sufix }) => (
+  <div className="container header-content">
+    <div className="row">
+      <div className="col-md-12 col-sm-12 text-center">
+        <div className="contents">
+          <h5 className="script-font wow fadeInUp" data-wow-delay="0.2s">
+            {prefix}
+          </h5>
+          <h2 className="head-title wow fadeInUp" data-wow-delay="0.4s">
+            {name}
+          </h2>
+          <p className="script-font wow fadeInUp" data-wow-delay="0.6s">
+            {sufix}
+          </p>
+          <ul className="social-icon wow fadeInUp" data-wow-delay="0.8s">
+            <SocialLinksContainer />
+          </ul>
+          <div className="header-button wow fadeInUp" data-wow-delay="1s">
+            {/* <a href="#" className="btn btn-common">
+          Get a Free Quote
+        </a> */}
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+);
+
 const Header = () => {
   return (
     <>
       <Navbar />
+
       <header id="header-wrap">
-        {/* <!-- Hero Area Start --> */}
         <div id="hero-area" className="hero-area-bg">
-          <div className="overlay"></div>
-          <div className="container">
-            <div className="row">
-              <div className="col-md-12 col-sm-12 text-center">
-                <div className="contents">
-                  <h5
-                    className="script-font wow fadeInUp"
-                    data-wow-delay="0.2s"
-                  >
-                    Hi This is
-                  </h5>
-                  <h2 className="head-title wow fadeInUp" data-wow-delay="0.4s">
-                    Felipe Souza
-                  </h2>
-                  <p className="script-font wow fadeInUp" data-wow-delay="0.6s">
-                    {"I'm a Front-end Software Developer"}
-                  </p>
-                  <ul
-                    className="social-icon wow fadeInUp"
-                    data-wow-delay="0.8s"
-                  >
-                    <SocialLinksContainer />
-                  </ul>
-                  <div
-                    className="header-button wow fadeInUp"
-                    data-wow-delay="1s"
-                  >
-                    <a href="#" className="btn btn-common">
-                      Get a Free Quote
-                    </a>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
+          <div className="overlay" />
+          <ContainerContent
+            prefix="Heya, This is"
+            name="Felipe Souza"
+            sufix="I'm Front-end Software Developer"
+          />
         </div>
       </header>
     </>
   );
+};
+
+ContainerContent.propTypes = {
+  prefix: PropTypes.string.isRequired,
+  sufix: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
 };
 
 export default Header;
